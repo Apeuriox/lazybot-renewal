e15c595f24da9aaf5b73da364938c2508e35af84
Aloic <liuxusiu@foxmail.com>
2025-01-18 00:46:38 +0800

    broken shiro test code above

--- src/main/java/me/aloic/lazybot/LazybotJdaApplication.java
+++ src/main/java/me/aloic/lazybot/LazybotJdaApplication.java
@@ -1,9 +1,13 @@
 package me.aloic.lazybot;
 
+import com.mikuac.shiro.annotation.common.Shiro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.LoggerContext;
+import org.slf4j.LoggerFactory;
 
 @EnableAsync
 @EnableScheduling
@@ -13,6 +17,7 @@ public class LazybotJdaApplication
 
     public static void main(String[] args)
     {
+        ((LoggerContext) LoggerFactory.getILoggerFactory()).getLogger("com.mikuac.shiro").setLevel(Level.DEBUG);
         SpringApplication.run(LazybotJdaApplication.class, args);
     }
 
--- src/main/java/me/aloic/lazybot/shiro/listener/CommandListener.java
+++ src/main/java/me/aloic/lazybot/shiro/listener/CommandListener.java
@@ -1,10 +1,11 @@
 package me.aloic.lazybot.shiro.listener;
 
 import com.mikuac.shiro.annotation.AnyMessageHandler;
+import com.mikuac.shiro.annotation.GroupMessageHandler;
 import com.mikuac.shiro.annotation.MessageHandlerFilter;
 import com.mikuac.shiro.annotation.common.Shiro;
 
-import com.mikuac.shiro.common.utils.MsgUtils;
+import com.mikuac.shiro.common.utils.ShiroUtils;
 import com.mikuac.shiro.core.Bot;
 import com.mikuac.shiro.core.BotPlugin;
 import com.mikuac.shiro.dto.event.message.AnyMessageEvent;
@@ -12,45 +13,33 @@ import com.mikuac.shiro.dto.event.message.AnyMessageEvent;
 import com.mikuac.shiro.dto.event.message.GroupMessageEvent;
 import com.mikuac.shiro.dto.event.message.PrivateMessageEvent;
 import com.mikuac.shiro.enums.ReplyEnum;
+import jakarta.annotation.Resource;
+import me.aloic.lazybot.component.SlashCommandProcessor;
 import me.aloic.lazybot.discord.config.DiscordBotRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 
 @Shiro
 @Component
-public class CommandListener extends BotPlugin
+public class CommandListener
 {
     private static final Logger logger = LoggerFactory.getLogger(CommandListener.class);
-    @Override
-    public int onPrivateMessage(Bot bot, PrivateMessageEvent event) {
-        logger.info("recive private message: {}", event.getMessage());
-        if ("ping".equals(event.getMessage())) {
-            // 构建消息
-            String sendMsg = MsgUtils.builder()
-                    .text("pong!")
-                    .build();
-            // 发送私聊消息
-            bot.sendPrivateMsg(event.getUserId(), sendMsg, false);
-        }
-        // 返回 MESSAGE_IGNORE 执行 plugin-list 下一个插件，返回 MESSAGE_BLOCK 则不执行下一个插件
-        return MESSAGE_IGNORE;
+
+    @Resource
+    private SlashCommandProcessor slashCommandProcessor;
+
+    @GroupMessageHandler
+    @Async
+    public void onSlashCommandInteraction(Bot bot, GroupMessageEvent event) {
+        logger.trace("收到消息[{}] -> {}", event.getGroupId(), ShiroUtils.unescape(event.getRawMessage()));
+
+
+
     }
 
-    @Override
-    public int onGroupMessage(Bot bot, GroupMessageEvent event) {
-        if ("ping".equals(event.getMessage())) {
-            // 构建消息
-            String sendMsg = MsgUtils.builder()
-                    .text("pong!")
-                    .build();
-            // 发送群消息
-            bot.sendGroupMsg(event.getGroupId(), sendMsg, false);
-        }
-        // 返回 MESSAGE_IGNORE 执行 plugin-list 下一个插件，返回 MESSAGE_BLOCK 则不执行下一个插件
-        return MESSAGE_IGNORE;
-    }
 }
 
 
